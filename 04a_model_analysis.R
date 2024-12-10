3e7a04ed3686ba8e324e50c4b9a52dd1a231716f
Ali Slayie <ali2slayie@gmail.com>
2024-12-10 14:10:52 -0600

    calculated accuracy of models and created autoplot where applicable

---
+++ 04a_model_analysis.R
@@ -0,0 +1,86 @@
+# Analysis of trained models (comparisons)
+# Select final model
+# Fit & analyze final model
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# set seed
+set.seed(301)
+
+# load data, recipes and fits
+load(here("results/data_split.rda"))
+load(here("recipes/ks_recipes.rda"))
+
+load(here("results/a_logistic_fit.rda"))
+load(here("results/a_dtree_fit.rda"))
+load(here("results/a_nbayes_fit.rda"))
+load(here("results/a_knn_fit.rda"))
+load(here("results/a_rulefit_fit.rda"))
+
+
+library(doMC)
+registerDoMC(cores = parallel::detectCores(logical =TRUE))
+
+# results
+
+logistic_result <- collect_metrics(logistic_fit) |> 
+  mutate(model = "logistic")
+logistic_result
+
+dtree_result <- collect_metrics(dtree_fit) |> 
+  mutate(model = "decision tree")
+dtree_result
+
+knn_result <- collect_metrics(knn_fit) |> 
+  mutate(model = "KNN")
+knn_result
+
+nbayes_result <- collect_metrics(nbayes_fit) |> 
+  mutate(model = "Naive Bayes")
+nbayes_result
+
+rulefit_result <- collect_metrics(rulefit_fit) |> 
+  mutate(model = "RuleFit")
+rulefit_result
+
+log_a_acc <- logistic_result |> 
+  filter(.metric == "accuracy") |> 
+  slice_max(mean) |> 
+  select(mean, n, std_err) |> 
+  mutate(model = "Logistic KS")
+
+dtree_a_acc <- dtree_result |> 
+  filter(.metric == "accuracy") |> 
+  slice_max(mean) |> 
+  select(mean, n, std_err) |> 
+  mutate(model = "Decision Tree KS")
+
+knn_a_acc <- knn_result |> 
+  filter(.metric == "accuracy") |> 
+  slice_max(mean) |> 
+  select(mean, n, std_err) |> 
+  mutate(model = "KNN KS")
+
+nbayes_a_acc <- nbayes_result |> 
+  filter(.metric == "accuracy") |> 
+  slice_max(mean) |> 
+  select(mean, n, std_err) |> 
+  mutate(model = "Naive Bayes KS")
+
+rulefit_a_acc <- rulefit_result |> 
+  filter(.metric == "accuracy") |> 
+  slice_max(mean) |> 
+  select(mean, n, std_err) |> 
+  mutate(model = "RuleFit KS")
+
+
+table_a_accuracy <- bind_rows(log_a_acc, dtree_a_acc, knn_a_acc, nbayes_a_acc, rulefit_a_acc) |> 
+  select(model, mean, std_err, n) |> 
+  arrange(mean)
+table_a_accuracy
