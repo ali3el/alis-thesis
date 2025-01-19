79aff0cfee6fc8ed82771c59664cfa7934f93b14
Ali Slayie <ali2slayie@gmail.com>
2025-01-19 13:11:25 -0800

    ran models

--- attempt_3/4c_model_analysis.R
+++ attempt_3/4c_model_analysis.R
@@ -14,14 +14,14 @@ tidymodels_prefer()
 set.seed(301)
 
 # load data, recipes and fits
-load(here("attempt_2/results/data_split.rda"))
-load(here("attempt_2/recipes/m1_recipes.rda"))
+load(here("attempt_3/results/data_split.rda"))
+load(here("attempt_3/recipes/m3_recipes.rda"))
 
-load(here("attempt_2/results/b_logistic_fit.rda"))
-load(here("attempt_2/results/b_dtree_fit.rda"))
-load(here("attempt_2/results/b_nbayes_fit.rda"))
-load(here("attempt_2/results/b_knn_fit.rda"))
-load(here("attempt_2/results/b_rulefit_fit.rda"))
+load(here("attempt_3/results/c_logistic_fit.rda"))
+load(here("attempt_3/results/c_dtree_fit.rda"))
+load(here("attempt_3/results/c_nbayes_fit.rda"))
+load(here("attempt_3/results/c_knn_fit.rda"))
+load(here("attempt_3/results/c_rulefit_fit.rda"))
 
 
 library(doMC)
@@ -29,71 +29,71 @@ registerDoMC(cores = parallel::detectCores(logical =TRUE))
 
 # results
 
-logistic_result <- collect_metrics(logistic_fit_b) |> 
+logistic_result <- collect_metrics(logistic_fit_c) |> 
   mutate(model = "logistic")
 logistic_result
 
-dtree_result <- collect_metrics(dtree_fit_b) |> 
+dtree_result <- collect_metrics(dtree_fit_c) |> 
   mutate(model = "decision tree")
 dtree_result
 
-knn_result <- collect_metrics(knn_fit_b) |> 
+knn_result <- collect_metrics(knn_fit_c) |> 
   mutate(model = "KNN")
 knn_result
 
-nbayes_result <- collect_metrics(nbayes_fit_b) |> 
+nbayes_result <- collect_metrics(nbayes_fit_c) |> 
   mutate(model = "Naive Bayes")
 nbayes_result
 
-rulefit_result <- collect_metrics(rulefit_fit_b) |> 
+rulefit_result <- collect_metrics(rulefit_fit_c) |> 
   mutate(model = "RuleFit")
 rulefit_result
 
-log_b_acc <- logistic_result |> 
+log_c_acc <- logistic_result |> 
   filter(.metric == "accuracy") |> 
   slice_max(mean) |> 
   select(mean, n, std_err) |> 
-  mutate(model = "Logistic M1")
+  mutate(model = "Logistic M3")
 
-dtree_b_acc <- dtree_result |> 
+dtree_c_acc <- dtree_result |> 
   filter(.metric == "accuracy") |> 
   slice_max(mean) |> 
   select(mean, n, std_err) |> 
-  mutate(model = "Decision Tree M1")
+  mutate(model = "Decision Tree M3")
 
-knn_b_acc <- knn_result |> 
+knn_c_acc <- knn_result |> 
   filter(.metric == "accuracy") |> 
   slice_max(mean) |> 
   select(mean, n, std_err) |> 
-  mutate(model = "KNN M1")
+  mutate(model = "KNN M3")
 
-nbayes_b_acc <- nbayes_result |> 
+nbayes_c_acc <- nbayes_result |> 
   filter(.metric == "accuracy") |> 
   slice_max(mean) |> 
   select(mean, n, std_err) |> 
-  mutate(model = "Naive Bayes M1")
+  mutate(model = "Naive Bayes M3")
 
-rulefit_b_acc <- rulefit_result |> 
+rulefit_c_acc <- rulefit_result |> 
   filter(.metric == "accuracy") |> 
   slice_max(mean) |> 
   select(mean, n, std_err) |> 
-  mutate(model = "RuleFit M1")
+  mutate(model = "RuleFit M3")
 
 
-table_b_accuracy <- bind_rows(log_b_acc, dtree_b_acc, knn_b_acc, nbayes_b_acc, rulefit_b_acc) |> 
+table_c_accuracy <- bind_rows(log_c_acc, dtree_c_acc, knn_c_acc, nbayes_c_acc, rulefit_c_acc) |> 
   select(model, mean, std_err, n) |> 
   arrange(mean) |> 
   distinct()
-table_b_accuracy
+table_c_accuracy
 
-dtree_autoplot_b <- autoplot(dtree_fit_b, metric = "accuracy")
-knn_autoplot_b <- autoplot(knn_fit_b, metric = "accuracy")
-rulefit_autoplot_b <- autoplot(rulefit_fit_b, metric = "accuracy")
+dtree_autoplot_c <- autoplot(dtree_fit_c, metric = "accuracy")
+knn_autoplot_c <- autoplot(knn_fit_c, metric = "accuracy")
+rulefit_autoplot_c <- autoplot(rulefit_fit_c, metric = "accuracy")
 # nbayes_autoplot_a <- autoplot(nbayes_fit, metric = "accuracy")
 
-save(table_b_accuracy, dtree_autoplot_b,
-     knn_autoplot_b, rulefit_autoplot_b,
-     file = here("attempt_2/results/metric_results_b.rda"))
+save(table_c_accuracy, dtree_autoplot_c,
+     knn_autoplot_c, rulefit_autoplot_c,
+     file = here("attempt_3/results/metric_results_c.rda"))
 
 
 
